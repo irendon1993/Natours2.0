109a63dfd0797c6cc3865599a45e83ac93c9ad05
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-02-27 12:35:45 -0500

    Creating Tour Documents

--- server.js
+++ server.js
@@ -37,6 +37,21 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  // rating: 4.7,
+  price: 997
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR:', err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
